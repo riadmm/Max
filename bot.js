e655b57a5566c1d74f6610f8d6b89ba0b15eee0b
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-08-31 07:57:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,86 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('Pong!');
-  }
-});
-
-
-
-
-
-
-client.on('guildMemberAdd', (member) => {
-let channel = client.channels.get('470573951777767444')
-if(member.user.bot) {
-channel.send(`${member} واو جانا بوت شباب يلا نستمتع`)
-}
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription(" ✅   :heart:البوت ما يرفضلك طلب يا قلبي عطاك ياه خاص ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
-
--${message.guild.name}  Link
-**`)
-      message.author.sendEmbed(Embed11)
-    }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
 
 
 
@@ -132,7 +52,7 @@ client.on('ready', () => {
 
 
 client.on('ready', () => {
-client.user.setGame(`HD SYSTEM`,'https://www.twitch.tv/hmamkhalid');                   client.user.setGame(`بوت خيالي`,'https://www.twitch.tv/hmamkhalid');
+client.user.setGame(`بيني وبينك القاء واحد`,'https://www.twitch.tv/hmamkhalid');                   client.user.setGame(`بوت خيالي`,'https://www.twitch.tv/hmamkhalid');
 });
 
 
@@ -169,82 +89,11 @@ m.sendMessage(args)
 
 
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`:rose:  ولكم نورت السيرفر:rose:
-:crown:اسم العضو  ${member}:crown:
-انت العضو رقم ${member.guild.memberCount} `)
-}).catch(console.error)
-})
 
 
 
 
 
-
-
-client.on('message', message => {
-            if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('!bco')){
- if(!message.author.id === '394715584421429260') return;
-message.channel.sendMessage('جار ارسال الرسالة |✅')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'chat');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return;
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField('🎽 | name :  ',`${member}`)
-        .addField('📢 | نورت السيرفر يا قلبي' , `Welcome to the server, ${member}`)
-        .addField('🆔 | user :', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                     
-                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                       
-     .setFooter(`${member.guild.name}`)
-        .setTimestamp()
-   
-      channel.sendEmbed(embed);
-    });
-    
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`الله معاك ✋:skin-tone-1: 😔`)
-        .setDescription(`مع السلامه تشرفنا بك ✋:skin-tone-1: 😔 `)
-        .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('RED')
-        .setFooter(`==== نــتــمــنــآ لــكــم آســتــمـــتــآع ====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-    
-    var channel =member.guild.channels.find('name', 'chat')
-    if (!channel) return;
-    channel.send({embed : embed});
-    })
 
 
 
