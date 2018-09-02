ec68b16a11c58bb1c5a534ec7ec067c5c7736e28
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-09-02 10:04:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,24 +33,6 @@ client.on('message', msg => {
 
 
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-if (command == "_embed") {
-    let say = new Discord.RichEmbed()
-  .setThumbnail(message.author.avatarURL)
-  .setAuthor(message.author.username)
-    .setDescription(args.join("  "))
-    .setColor(0x06DF00)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-  });
 
 
 
