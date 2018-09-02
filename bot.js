1be33f2d4b7a3b29637bdb66089b581063eaedb5
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-09-02 14:28:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,18 +32,7 @@ client.on('message', msg => {
 });
 
 
- client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-   let args = message.content.split(" ").slice(1);
-   if (command == "-say") {
- if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('**يلزم لهذآ ألأمر أن تكون من ألإدآرة أو لديك برمشن** `ADMINISTRATOR`' );
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
+
 
 
 
