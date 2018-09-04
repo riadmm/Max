bb4ff80802b64fb5463c0f744ecba96314aa64f0
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-09-04 07:46:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,18 +97,20 @@ client.on('message', message => {
 
 
 
+
 client.on("message", async message => {
 if(message.content.startsWith("-embed")) {
 let args = message.content.split(" ").slice(1).join(" ")
-if(message.member.id != "394715584421429260") return;
 if(!args) return message.reply("Romz");
 message.delete();
 var emb = new  Discord.RichEmbed()
-  .setColor('red')
+  .setColor('RANDOM')
   .setDescription(args);
 message.channel.send(emb)
 }
 })
+
+    
 
 
 
