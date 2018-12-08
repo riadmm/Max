a5ac6603aa5b4d22ad314cd1ddf4314e56daf89f
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-12-08 16:46:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ client.on('message', message => {
     if (message.guild) {
    let embed = new Discord.RichEmbed()
     let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + '+bc') {
+if(message.content.split(' ')[0] == (prefix + '+bc') {
     if (!args[1]) {
 return;
 }
