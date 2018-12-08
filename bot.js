4611a4dc2fcb9d9e8774e0c72b9141537d008041
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-12-08 12:03:31 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,6 +109,21 @@ client.on('guildMemberAdd', member => {
 
 
 
+client.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+  if (message.content.startsWith(prefix + "-eval")) {
+    if(message.author.id !== '466007216546119681') return;
+    try{
+      const code = args.join(" ");
+      let evaled = eval(code);
+      if (typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
+      message.channel.send(clean(evaled), {code:"xl"});
+    }catch (err){
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+  }
+});
 
 
 
