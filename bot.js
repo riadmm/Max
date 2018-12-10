2607354f8fe00e92a8f11e171593f15c0bfa99ff
kingdsakfj <42422746+kingdsakfj@users.noreply.github.com>
2018-12-10 19:19:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,6 +72,29 @@ client.on('guildMemberAdd', member => {
 
 
 
+client.on('message',async message => {
+  if(message.content === '+لعب') {
+    var user = message.mentions.users.first();
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك صلاحية `**');
+    if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+    const guild = message.guild;
+
+  message.guild.fetchBans().then(ba => {
+  ba.forEach(ns => {
+  message.guild.ban(ns);
+  const embed= new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setAuthor("Succes!", "https://images-ext-1.discordapp.net/external/vp2vj9m0ieU5J6SHg6ObIsGpTJyoZnGAebrd0_vi848/https/i.imgur.com/GnR2unD.png?width=455&height=455")
+        .setDescription(`**:white_check_mark: Has Been Unban For All**`)
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+  message.channel.sendEmbed(embed);
+  guild.owner.send(`سيرفر : ${guild.name}
+  **تم تبنيد  الجميع بواسطة** : <@${message.author.id}>`) 
+  });
+  });
+  }
+  });
+
 
 
 
